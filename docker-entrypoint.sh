392f5b9a5ba25da5a8de615c8aa3828ae4cea7f0
ljmict <ljmict@163.com>
2019-12-12 19:48:07 +0800

    test

--- docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mv /apps/php-7.4.0/etc/php-fpm.conf.default /apps/php-7.4.0/etc/php-fpm.conf
-mv /apps/php-7.4.0/etc/php-fpm.d/www.conf.default /apps/php-7.4.0/etc/php-fpm.d/www.conf
-exec "$@"
